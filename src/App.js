00d65ae7e6597db5386932b39c0f497ee4e31387
Hossam Morsey <hosammorsey@gmail.com>
2022-01-15 18:29:03 +0300

    [Create] create react app for hubspot challenge.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { hubSpotLogo, background } from "./utils/utils";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2> Welcome to HubSpot challenge</h2>
+        <img src={hubSpotLogo} alt="Main Icon" />
+
+        <img src={background} alt="Main Icon" />
+
       </header>
     </div>
   );
---
+++ src/utils/utils.js
@@ -0,0 +1,3 @@
+// export {default as hubSpotLogo} from "../assets/icons/";
+export {default as hubSpotLogo} from "./../assets/hubspot-logo.png"
+export {default as background} from "./../assets/hubspot-background.png"
